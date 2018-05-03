f7c371406ec5e597cdf37f2b8e4334e627db871c
CrabAss <jan.hanyu@outlook.com>
2018-05-03 21:29:05 +0800

    code readability improved

--- py-version/main.py
+++ py-version/main.py
@@ -14,47 +14,57 @@ class NoRedirection(urllib.request.HTTPErrorProcessor):
 
 
 opener = urllib.request.build_opener(NoRedirection)
+block_size = 1024 * 100  # 100KB
+
+
+def percentage(a, b=100, round_digit=2):
+    return str(round(a * 100 / b, round_digit)) + "%"
+
+
+def check_url(url):
+    redirect_count = 0
+    while True:  # Stage 1: Remote file detection
+        res = opener.open(urllib.request.Request(url, method="HEAD"))
+        if res.getcode() >= 400:  # Error handling
+            raise Exception("Server returned an error! Error code: " + str(res.getcode()))
+        elif res.getcode() >= 300:  # Redirect handling
+            if redirect_count < 20:
+                url = res.geturl()
+                redirect_count += 1
+            else:
+                raise Exception("Too many times of redirection. ")
+        else:  # Success
+            return res
 
 
 def download(url="http://158.132.255.107:25003/project/team1.txt", file_path=None, timeout=10):
 
-    if file_path is None:
-        remote_filename = url.split("/")[-1].split("?")[0]
-        if remote_filename == "":
-            remote_filename = "index"
-        file_path = "./" + remote_filename
-
-    if os.path.exists(file_path):
-        print("File \"" + file_path.split("/")[-1] + "\" already existed!")
-        if input('Do you want to delete and re-download it [Y/N]? ').lower() != 'y':
-            print("The program is exiting according to your instruction. ")
-            return
-        else:
-            os.remove(file_path)
-
-    block_size = 1024 * 100  # 100KB
-    tmp_file_path = file_path + ".part"
     file_size = -1
+    tmp_file_path = None
 
     try:
-        redirect_count = 0
-        while True:
-            first_response = opener.open(urllib.request.Request(url, method="HEAD"))
-            if first_response.getcode() >= 400:
-                print("Server returned an error! Error code: " + str(first_response.getcode()))
-                return
-            elif first_response.getcode() >= 300:
-                if redirect_count < 20:
-                    url = first_response.geturl()
-                    redirect_count += 1
-                else:
-                    raise Exception("Too many times of redirection. ")
-            else:
-                break
+        first_response = check_url(url)
+        url = first_response.geturl()
 
-        # tmp file
+        if file_path is None:   # Extract remote filename automatically
+            remote_filename = url.split("/")[-1].split("?")[0]
+            if remote_filename == "":
+                remote_filename = "index"
+            file_path = "./" + remote_filename
+
+        if os.path.exists(file_path):
+            print("File \"" + file_path.split("/")[-1] + "\" already existed!")
+            if input('Do you want to delete and re-download it [Y/N]? ').lower() != 'y':
+                print("The program is exiting according to your instruction. ")
+                return
+            else:
+                os.remove(file_path)
+
+        tmp_file_path = file_path + ".part"     # temp file
+
         print("\nRemote URL: " + str(url))
 
+        # Generate human-readable file size
         file_size = int(first_response.info().get("Content-Length", -1))
         readable_size = file_size
         readable_size_unit = 0
@@ -64,15 +74,16 @@ def download(url="http://158.132.255.107:25003/project/team1.txt", file_path=Non
         readable_size = str(round(readable_size, 2)) + " " + size_unit[readable_size_unit]
         print("File size: " + readable_size)
 
+        is_resumable = first_response.info().get("Accept-Ranges", "none") != "none"
+        if not is_resumable:
+            print("Server does not allow resumable downloading. ")
+
         first_byte = os.path.getsize(tmp_file_path) if os.path.exists(tmp_file_path) else 0
-        print("Downloading with urllib...\n")
+        print("Downloading with urllib from " + percentage(first_byte, file_size) + "...\n")
 
-
-        if first_response.info().get("Accept-Ranges", "none") == "none":
+        if not is_resumable:
             print("Downloading... ", end="")
-            headers = {
-                "Group": 1
-            }
+            headers = {"Group": 1}
             req = urllib.request.Request(url, headers=headers)
             page = opener.open(req, timeout=timeout).read()
             print("OK")
@@ -93,7 +104,7 @@ def download(url="http://158.132.255.107:25003/project/team1.txt", file_path=Non
                     }
                     req = urllib.request.Request(url, headers=headers)
                     page = opener.open(req, timeout=timeout).read()
-                    print(str(round(last_byte * 100 / file_size, 2)) + "%")
+                    print(percentage(last_byte, file_size))
 
                     with open(tmp_file_path, "ab") as f:
                         f.write(page)
@@ -107,7 +118,7 @@ def download(url="http://158.132.255.107:25003/project/team1.txt", file_path=Non
     except urllib.error.URLError as e:
         print("Error! Reason:", e.reason)
     except Exception as e:
-        print('Unexpected error!')
+        print("Error: ", end="")
         print(e)
     finally:
         # rename the temp download file to the correct name if fully downloaded
